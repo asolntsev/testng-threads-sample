86de437d676437bd974d7d74b52e0d8b8a6c7a9d
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-01-29 13:27:58 +0200

    add "implements IInvokedMethodListener"
    It didn't help: new methods "beforeInvocation" and "afterInvocation" are still executed in different thread (compared to test).

--- src/test/java/io/github/asolntsev/MyListener.java
+++ src/test/java/io/github/asolntsev/MyListener.java
@@ -6,11 +6,22 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class MyListener implements ITestListener {
+public class MyListener implements ITestListener, IInvokedMethodListener {
   private static final Logger log = LoggerFactory.getLogger(MyListener.class);
+
+  @Override public void beforeInvocation(IInvokedMethod method, ITestResult testResult, ITestContext context) {
+    log.info("Before invocation {} in {}", context.getName(), currentThread().getName());
+  }
+
+  @Override public void afterInvocation(IInvokedMethod method, ITestResult testResult, ITestContext context) {
+    log.info("After invocation {} in {}", context.getName(), currentThread().getName());
+  }
 
   @Override
   public void onTestStart(ITestResult result) {
--- src/test/java/io/github/asolntsev/WithTimeoutTest.java
+++ src/test/java/io/github/asolntsev/WithTimeoutTest.java
@@ -1,13 +1,20 @@
 package io.github.asolntsev;
 
+import static java.lang.Thread.currentThread;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners(MyListener.class)
 public class WithTimeoutTest {
+  private static final Logger log = LoggerFactory.getLogger(WithTimeoutTest.class);
 
   @Test
   public void myTest() {
+    log.info("run test 'myTest' in {}", currentThread().getName());
+    
     if (Math.random() > -0.001) {
       ErrorsCollector.addError("One");
     }
@@ -15,6 +22,7 @@ public class WithTimeoutTest {
 
   @Test(timeOut = 99999)
   public void myTestWithTimeout() {
+    log.info("run test 'myTestWithTimeout' in {}", currentThread().getName());
     if (Math.random() > -0.001) {
       ErrorsCollector.addError("Two");
     }
